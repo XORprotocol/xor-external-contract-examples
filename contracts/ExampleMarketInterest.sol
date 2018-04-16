abf7095c71e5b24e83b2d459bfb6ce46c4debfe9
Matthew Black <tcai534@gmail.com>
2018-04-15 23:41:37 -0700

    removed import xorMath for ExampleInterest

--- contracts/ExampleMarketInterest.sol
+++ contracts/ExampleMarketInterest.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/lifecycle/Destructible.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import 'xor-libraries/contracts/XorMath.sol';
 
 /**
   * @title MarketInterestInterface
