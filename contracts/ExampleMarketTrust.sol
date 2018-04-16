16ea71e9aebe1ca98d7cefa8db19036e9ed043d0
Matthew Black <tcai534@gmail.com>
2018-04-15 20:19:47 -0700

    bug fixes

--- contracts/ExampleMarketTrust.sol
+++ contracts/ExampleMarketTrust.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/lifecycle/Destructible.sol';
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import './libraries/XorMath.sol';
 
 /**
